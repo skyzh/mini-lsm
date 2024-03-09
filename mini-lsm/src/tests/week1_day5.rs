6bfff2131b17fea9cba9109846628281a55280bc
Zixuan Chen <remch183@outlook.com>
2024-03-09 22:46:19 +0800

    test: test scan exclusive start on sstable

--- mini-lsm/src/tests/week1_day5.rs
+++ mini-lsm/src/tests/week1_day5.rs
@@ -187,6 +187,21 @@ fn test_task2_storage_scan() {
             .unwrap(),
         vec![(Bytes::from("2"), Bytes::from("2333"))],
     );
+    check_lsm_iter_result_by_key(
+        &mut storage
+            .scan(Bound::Included(b"0"), Bound::Included(b"1"))
+            .unwrap(),
+        vec![
+            (Bytes::from_static(b"0"), Bytes::from_static(b"2333333")),
+            (Bytes::from("00"), Bytes::from("2333")),
+        ],
+    );
+    check_lsm_iter_result_by_key(
+        &mut storage
+            .scan(Bound::Excluded(b"0"), Bound::Included(b"1"))
+            .unwrap(),
+        vec![(Bytes::from("00"), Bytes::from("2333"))],
+    );
 }
 
 #[test]
