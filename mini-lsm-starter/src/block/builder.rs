3d48514e6e4426a6280f944efe7e1a02022d4d27
Pratik Agarwal <pratik@momentohq.com>
2024-07-28 00:39:10 -0700

    feat: week 1, day 3, task 1

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,9 +1,15 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use crate::key::{KeySlice, KeyVec};
+use bytes::BufMut;
+
+use crate::key::{Key, KeySlice, KeyVec};
 
 use super::Block;
+
+pub(crate) const U16_SIZE: usize = std::mem::size_of::<u16>();
+
+const NUM_ELEMENTS_SIZE: usize = U16_SIZE;
 
 /// Builds a block.
 pub struct BlockBuilder {
@@ -20,22 +26,56 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: Key::new(),
+        }
+    }
+
+    pub fn size(&mut self) -> usize {
+        NUM_ELEMENTS_SIZE + self.data.len() + (self.offsets.len() * U16_SIZE)
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        let estimated_size_for_data = key.len() + value.len();
+        let estimated_size_for_metadata = 3 * U16_SIZE;
+
+        if self.size() + estimated_size_for_data + estimated_size_for_metadata > self.block_size {
+            if !self.is_empty() {
+                return false;
+            }
+        }
+
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16(key.len() as u16);
+        self.data.put(key.into_inner());
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+
+        if self.first_key.is_empty() {
+            self.first_key = key.to_key_vec();
+        }
+
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.first_key.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        if self.is_empty() {
+            panic!("Block is empty!");
+        }
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
--- mini-lsm-starter/src/tests.rs
+++ mini-lsm-starter/src/tests.rs
@@ -1,6 +1,7 @@
 //! DO NOT MODIFY -- Mini-LSM tests modules
 //! This file will be automatically rewritten by the copy-test command.
 
+mod week1_day2;
+mod week1_day3;
 mod harness;
 mod week1_day1;
-mod week1_day2;
