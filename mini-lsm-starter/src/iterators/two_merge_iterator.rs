55b6acf030dc92be35c28f5eaecba510137f2ba8
AmberWagner <wzhzzmzzy@gmail.com>
2024-09-24 19:54:32 +0800

    day5/task1

--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -5,12 +5,18 @@ use anyhow::Result;
 
 use super::StorageIterator;
 
+#[derive(PartialEq, Debug)]
+enum Current {
+    A = 0,
+    B = 1,
+}
+
 /// Merges two iterators of different types into one. If the two iterators have the same key, only
 /// produce the key once and prefer the entry from A.
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
-    // Add fields as need
+    current: Current,
 }
 
 impl<
@@ -19,7 +25,20 @@ impl<
     > TwoMergeIterator<A, B>
 {
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        let current = if !a.is_valid() || !b.is_valid() {
+            if a.is_valid() {
+                Current::A
+            } else {
+                Current::B
+            }
+        } else {
+            if a.key() > b.key() {
+                Current::B
+            } else {
+                Current::A
+            }
+        };
+        Ok(Self { a, b, current })
     }
 }
 
@@ -31,18 +50,55 @@ impl<
     type KeyType<'a> = A::KeyType<'a>;
 
     fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+        if self.current == Current::A {
+            self.a.key()
+        } else {
+            self.b.key()
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        if self.current == Current::A {
+            self.a.value()
+        } else {
+            self.b.value()
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        if self.current == Current::A {
+            self.a.is_valid()
+        } else {
+            self.b.is_valid()
+        }
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        if self.a.is_valid() && self.b.is_valid() && self.a.key() == self.b.key() {
+            self.a.next()?;
+            self.b.next()?;
+        } else if self.current == Current::A {
+            self.a.next()?;
+        } else {
+            self.b.next()?;
+        }
+
+        if !self.a.is_valid() || !self.b.is_valid() {
+            self.current = if self.a.is_valid() {
+                Current::A
+            } else {
+                Current::B
+            };
+
+            return Ok(());
+        }
+
+        self.current = if self.a.key() <= self.b.key() {
+            Current::A
+        } else {
+            Current::B
+        };
+
+        Ok(())
     }
 }
