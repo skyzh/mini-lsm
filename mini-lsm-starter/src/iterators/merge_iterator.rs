c1f2ba054cc8f56a0a1ff220b059995b751de5f1
秋月平 <58326657+0ceanCat@users.noreply.github.com>
2024-12-15 18:02:03 +0000

    Feature/week1 day2 task2 (#8)
    * week2 day2 task2 done
    
    * week2 day2 task2 done
    
    * update
    
    * push
    
    * push

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -1,10 +1,7 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
-use std::cmp::{self};
-use std::collections::BinaryHeap;
-
 use anyhow::Result;
+use std::cmp::{self};
+use std::collections::binary_heap::PeekMut;
+use std::collections::BinaryHeap;
 
 use crate::key::KeySlice;
 
@@ -45,7 +42,33 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        let heap_elements: Vec<HeapWrapper<I>> = iters
+            .into_iter()
+            .enumerate()
+            .filter_map(|(index, iterator)| {
+                if iterator.is_valid() {
+                    Some(HeapWrapper {
+                        0: index,
+                        1: iterator,
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        if heap_elements.is_empty() || heap_elements.iter().all(|iterator| !iterator.1.is_valid()) {
+            return MergeIterator {
+                current: None,
+                iters: BinaryHeap::new(),
+            };
+        }
+
+        let mut heap = BinaryHeap::from(heap_elements);
+        MergeIterator {
+            current: heap.pop(),
+            iters: heap,
+        }
     }
 }
 
@@ -55,18 +78,53 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current
+            .as_ref()
+            .map(|x| x.1.is_valid())
+            .unwrap_or(false)
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let mut current = self.current.as_mut().unwrap();
+        while let Some(mut inner) = self.iters.peek_mut() {
+            if !inner.1.is_valid() {
+                PeekMut::pop(inner);
+            } else if current.1.key() == inner.1.key() {
+                // Case 1: an error occurred when calling `next`.
+                if let e @ Err(_) = inner.1.next() {
+                    PeekMut::pop(inner);
+                    return e;
+                }
+                if !inner.1.is_valid() {
+                    PeekMut::pop(inner);
+                }
+            } else {
+                break;
+            }
+        }
+
+        current.1.next()?;
+
+        if !current.1.is_valid() {
+            if let Some(iter) = self.iters.pop() {
+                *current = iter;
+            }
+            return Ok(())
+        }
+
+        if let Some(mut inner) = self.iters.peek_mut() {
+            if *current < *inner {
+                std::mem::swap(current, &mut *inner);
+            }
+        }
+        Ok(())
     }
 }
--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -3,7 +3,7 @@
 use std::collections::HashMap;
 use std::ops::Bound;
 use std::path::{Path, PathBuf};
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 
 use anyhow::Result;
