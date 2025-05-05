d383d70763fb89338a4a9a3095015123a5915130
lxc <ppdoooog@gmail.com>
2025-05-05 17:38:02 +0800

    Update watermark.rs (#144)
    add function that tests require

--- mini-lsm-starter/src/mvcc/watermark.rs
+++ mini-lsm-starter/src/mvcc/watermark.rs
@@ -32,6 +32,10 @@ impl Watermark {
 
     pub fn remove_reader(&mut self, ts: u64) {}
 
+    pub fn num_retained_snapshots(&self) -> usize {
+        0
+    }
+
     pub fn watermark(&self) -> Option<u64> {
         Some(0)
     }
